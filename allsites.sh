0d429e586af88fc4b351d0ead7f26bac6a072425
SuchitReddi <suchit20016@gmail.com>
2024-02-22 18:24:08 +0530

    Moved notes to another repo

---
+++ allsites.sh
@@ -0,0 +1,3 @@
+echo "This script is for hosting the github pages site as it is on my raspberry pi"
+echo "This script assumes that you are running it from the root of main github site"
+git clone https://github.com/SuchitReddi/Notes.git
